67630b4c68228cf7438c21c1a74aac9bc68be3a3
Fauzanyan <ccdplayerx@gmail.com>
2023-05-23 23:10:53 +0700

    tambahan

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
           <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl mb-5">About</h1>
-          <h1 class="text-[#CDCDCD] font-medium text-justify md:text-lg lg:text-xl">Hallo, Nama saya Muhammad Fauzan Yanuar Putra, lulusan SMK jurusan Rekayasa Perangkat Lunak. Saya adalah seorang pengembang web yang menggunakan framework <a href="" class="text-[#38F8B3] cursor-pointer">Vue js</a> dan <a href="" class="text-[#38F8B3] cursor-pointer">Laravel</a>. Untuk skill lengkap yang saya miliki bisa dilihat di bagian <a href="" class="text-[#38F8B3] cursor-pointer">Skills</a>.</h1>
+          <h1 class="text-[#CDCDCD] font-medium text-justify md:text-lg lg:text-xl">Hallo, Nama saya Muhammad Fauzan Yanuar Putra, lulusan SMK jurusan Rekayasa Perangkat Lunak. Saya adalah seorang pengembang web yang menggunakan framework <a href="https://vuejs.org/" class="text-[#38F8B3] cursor-pointer">Vue js</a> dan <a href="https://laravel.com/" class="text-[#38F8B3] cursor-pointer">Laravel</a>. Untuk skill lengkap yang saya miliki bisa dilihat di bagian <a href="#skills" class="text-[#38F8B3] cursor-pointer">Skills</a>.</h1>
 
         </div>
 
-            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-[800px] m-auto">
+            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-full m-auto">
 
 
         
@@ -33,17 +33,17 @@ import HelloWorld from './components/HelloWorld.vue'
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
           <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl mb-5">Blog</h1>
           <div class="w-full md:flex">
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">30 Agustus 2021</p><h1 class="font-bold my-2">Cara Menjadi Front End Developer Dan Yang Harus Di Kuasai</h1><p class="text-[#38F8B3]">Read more -></p></div>
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">21 September 2021</p><h1 class="font-bold my-2">16 Aplikasi Text Editor yang bisa kalian pakai untuk ngoding di perangkat</h1><p class="text-[#38F8B3]">Read more -></p></div>
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">01 September 2021</p><h1 class="font-bold my-2">Cara Menjadi Back End Developer Dan Yang Harus Dikuasai</h1><p class="text-[#38F8B3]">Read more -></p></div>
+            <a href="https://rakuzancoding.blogspot.com/2021/08/eqq.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">30 Agustus 2021</p><h1 class="font-bold my-2">Cara Menjadi Front End Developer Dan Yang Harus Di Kuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
+            <a href="https://rakuzancoding.blogspot.com/2021/09/15-aplikasi-coding-yang-bisa-kalian.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">21 September 2021</p><h1 class="font-bold my-2">16 Aplikasi Text Editor yang bisa kalian pakai untuk ngoding di perangkat</h1><p class="text-[#38F8B3]">Read more -></p></a>
+            <a href="https://rakuzancoding.blogspot.com/2021/09/cara-menjadi-back-end-developer-dan.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">01 September 2021</p><h1 class="font-bold my-2">Cara Menjadi Back End Developer Dan Yang Harus Dikuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
           </div>
         </div>
 
-            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-[800px] m-auto">
+            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-full m-auto">
 
 
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
-          <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl ">Skillset (web development)</h1>
+          <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl " id="skills">Skillset (web development)</h1>
           <div>
             <div class="my-6">
               <h1 class="text-white text-lg font-bold mb-1">HTML and CSS</h1>
@@ -74,7 +74,7 @@ import HelloWorld from './components/HelloWorld.vue'
               <p class="text-gray-300">Git, SQL</p>
             </div>
 
-            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-[800px] m-auto">
+            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-full m-auto">
 
 
             <h1 class="text-white text-lg font-bold mb-1 my-6">Skillset (other)</h1>
@@ -88,15 +88,15 @@ import HelloWorld from './components/HelloWorld.vue'
           </div>
         </div>
 
-            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-[800px] m-auto">
+            <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-full m-auto">
 
 
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
           <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl mb-5">Contact</h1>
           <div class="w-full md:flex">
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Github</div>
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Whattsapp</div>
-            <div class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Linkind</div>
+            <a href="https://github.com/Fauzanyan" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Github</a>
+            <a href="https://www.instagram.com/fauzanyanuarputra/" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Instagram</a>
+            <a href="www.linkedin.com/in/muhammad-fauzan-046246253" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Linkind</a>
           </div>
         </div>
 
