5927aa6053e5fd1e98e68c0e6580e767025e50b1
Fauzanyan <ccdplayerx@gmail.com>
2023-05-24 09:12:41 +0700

    ubah ubah

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,24 @@
-<script>
+<script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue';
 
-export default {
-  mounted() {
-    // Menggunakan lifecycle hook `onMounted` untuk menjalankan kode setelah komponen dipasang
-    onMounted(() => {
-      // Set scroll position ke atas pada saat komponen dipasang
-      window.scrollTo(0, 0);
-    });
-  },
-};
+
 </script>
 
 <template>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-  <div class="w-[100%] ">
+  <div class="w-[100%] " >
 
-    <div class="awalan  naik-atas mt-[40vh] lg:mt-[30vh]  px-5">
-        <div class="flex justify-center">
+    <div class="awalan  px-5">
+        <div class="flex naik-atas justify-center" >
             <div>
-              <div class="masuk-kanan mb-[-10px] text-6xl md:text-7xl lg:text-8xl xl:text-9xl bg-transparent  font-bold">Creative</div>
+              <div class="masuk-kanan mb-[-10px] lg:mb-[-25px] text-6xl md:text-7xl lg:text-8xl xl:text-9xl bg-transparent  font-bold">Creative</div>
               <div class="masuk-kiri text-6xl md:text-7xl lg:text-8xl xl:text-9xl bg-transparent  font-bold ml-20">Developer</div>
             </div>
         </div>
 
 
-        <div class="baru-muncul border-2 border-white mt-[40px] m-auto w-[20px]"></div>
+        <div class="baru-muncul border-2 border-white lg:mt-[10vh] m-auto w-[20px]"></div>
 
 
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
@@ -37,16 +28,14 @@ export default {
         </div>
 
             <hr class=" baru-muncul border-[#5C5C5C] my-[50px] w-full m-auto">
-
-
         
 
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
           <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl mb-5">Blog</h1>
-          <div class="w-full md:flex">
-            <a href="https://rakuzancoding.blogspot.com/2021/08/eqq.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">30 Agustus 2021</p><h1 class="font-bold my-2">Cara Menjadi Front End Developer Dan Yang Harus Di Kuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
-            <a href="https://rakuzancoding.blogspot.com/2021/09/15-aplikasi-coding-yang-bisa-kalian.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">21 September 2021</p><h1 class="font-bold my-2">16 Aplikasi Text Editor yang bisa kalian pakai untuk ngoding di perangkat</h1><p class="text-[#38F8B3]">Read more -></p></a>
-            <a href="https://rakuzancoding.blogspot.com/2021/09/cara-menjadi-back-end-developer-dan.html" class="block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">01 September 2021</p><h1 class="font-bold my-2">Cara Menjadi Back End Developer Dan Yang Harus Dikuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
+          <div class="w-full md:flex ">
+            <a href="https://rakuzancoding.blogspot.com/2021/08/eqq.html"  class=" block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">30 Agustus 2021</p><h1 class="font-bold my-2">Cara Menjadi Front End Developer Dan Yang Harus Di Kuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
+            <a href="https://rakuzancoding.blogspot.com/2021/09/15-aplikasi-coding-yang-bisa-kalian.html"  class=" block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">21 September 2021</p><h1 class="font-bold my-2">16 Aplikasi Text Editor yang bisa kalian pakai untuk ngoding di perangkat</h1><p class="text-[#38F8B3]">Read more -></p></a>
+            <a href="https://rakuzancoding.blogspot.com/2021/09/cara-menjadi-back-end-developer-dan.html"  class=" block cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white]"><p class=" text-[12px] font-medium text-gray-400">01 September 2021</p><h1 class="font-bold my-2">Cara Menjadi Back End Developer Dan Yang Harus Dikuasai</h1><p class="text-[#38F8B3]">Read more -></p></a>
           </div>
         </div>
 
@@ -105,9 +94,9 @@ export default {
         <div class="baru-muncul w-[90%] lg:w-[800px]  mt-[50px] m-auto flex justify-center flex-col ">
           <h1 class="mt-1  font-bold text-white text-center text-lg md:text-2xl lg:text-3xl mb-5">Contact</h1>
           <div class="w-full md:flex">
-            <a href="https://github.com/Fauzanyan" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Github</a>
-            <a href="https://www.instagram.com/fauzanyanuarputra/" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Instagram</a>
-            <a href="www.linkedin.com/in/muhammad-fauzan-046246253" class="cursor-pointer w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Linkind</a>
+            <a href="https://github.com/Fauzanyan" class="cursor-pointer w-full md:mx-2 my-5 p-5 hover:bg-white hover:text-black hover:font-bold rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Github</a>
+            <a href="https://www.instagram.com/fauzanyanuarputra/" class="cursor-pointer hover:bg-white hover:text-black hover:font-bold w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Instagram</a>
+            <a href="https://www.linkedin.com/in/muhammad-fauzan-046246253" class="cursor-pointer hover:bg-white hover:text-black hover:font-bold w-full md:mx-2 my-5 p-5 rounded-lg text-white  border-2 border-[white] flex justify-center items-center">Linkind</a>
           </div>
         </div>
 
@@ -122,6 +111,7 @@ export default {
 </template>
 
 <style scoped>
+
 
 @keyframes masukKanan {
   0% {
@@ -148,10 +138,13 @@ export default {
 
 @keyframes naikAtas {
   0% {
-      margi-bottom: 50vh;
+        height: 100vh;
+
   }
   100% {
-      margin-top: 2%;
+        height: 36vh;
+        /* padding: 30px; */
+
   }
 }
 
@@ -166,14 +159,27 @@ export default {
 }
 
 .masuk-kanan {
-  animation: masukKanan 1.2s ease-in-out;
+  opacity: 0;
+  animation: masukKanan 1.5s ease-in-out;
+  animation-fill-mode: forwards;
+
 }
 
 .masuk-kiri {
-  animation: masukKiri 1.2s ease-in-out;
+  opacity: 0;
+  animation: masukKiri 1.5s ease-in-out;
+  animation-fill-mode: forwards;
 }
 
 .naik-atas {
+  /* transform: translateY(40vh); */
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  /* background-color: red; */
+  
   animation: naikAtas 1.2s ease-in;
   animation-delay: 1s;
   animation-fill-mode: forwards;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { gsap } from "gsap";
 
-const app = createApp(App)
+// Optionally, you can import specific GSAP plugins if needed
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-app.use(router)
+gsap.registerPlugin(ScrollTrigger);
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
